af96ba439c63be26baebedfbd95c84fde6afe594
niclas.lang <niclas.lang@lineas.de>
2021-03-25 17:51:11 +0100

    first commit

---
+++ projects/abcalendar-lib/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of abcalendar-lib
+ */
+
+export * from './lib/abcalendar-lib.service';
+export * from './lib/abcalendar-lib.component';
+export * from './lib/abcalendar-lib.module';
+export * from './lib/calendar/calendar.component';
